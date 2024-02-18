2f2a02670981f32bc8488052e94024843a547d88
Dev Keshwani <dev.keshwani345@gmail.com>
2024-02-18 20:42:28 +0530

    fix: added env

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   // * Ensures that the Flutter Widgets library is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
+  // * Loads the .env file
+  await dotenv.load(fileName: '.env');
+
   // * Initializes Firebase
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-    appId: '1:586074781896:android:c1f321d1c43246ccd78dfd',
-    messagingSenderId: '586074781896',
-    projectId: 'zitch-5f2b1',
-    apiKey: 'AIzaSyATx3dOxGOanlBrf50qcxKNnx6bkepnBME',
-  ));
+    options: FirebaseOptions(
+      appId: dotenv.env['APP_ID'] ?? '',
+      messagingSenderId: dotenv.env['MESSAGING_ID'] ?? '',
+      projectId: dotenv.env['PROJECT_ID'] ?? '',
+      apiKey: dotenv.env['API_KEY'] ?? '',
+    ),
+  );
 
   // * Crashlytics Initialization
   FlutterError.onError = (errorDetails) {
