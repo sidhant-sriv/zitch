24e84102c79249ea537541ad3bcdbef866d5270b
sidhant-sriv <sidhant.sriv@gmail.com>
2024-02-19 10:04:33 +0530

    feat: inference engine with grpc protocol

---
+++ classifier/client.py
@@ -0,0 +1,17 @@
+import grpc
+import inference_pb2
+import inference_pb2_grpc
+import time
+def run():
+    with open('image.jpg', 'rb') as f:
+        image_bytes = f.read()
+
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = inference_pb2_grpc.InferenceStub(channel)
+        response = stub.Predict(inference_pb2.Image(content=image_bytes))
+    s = time.time()
+    print('Prediction:', response.value)
+    print('Confidence:', response.confidence)
+    print('Time:', time.time()-s)
+if __name__ == '__main__':
+    run()
